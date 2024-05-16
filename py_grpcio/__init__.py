afb5a29054ce3707008b9da294c9c2ad62bb84a5
Yurii <ykolibroda@lytvynov-production.com>
2024-05-16 23:23:11 +0100

    Updated version

--- py_grpcio/__init__.py
+++ py_grpcio/__init__.py
@@ -2,3 +2,7 @@ from py_grpcio.models import Message
 from py_grpcio.server import BaseServer
 from py_grpcio.service import BaseService
 from py_grpcio.middleware import BaseMiddleware
+
+from py_grpcio.__meta__ import __version__
+
+__all__: list[str] = ['BaseServer', 'BaseService', 'BaseMiddleware', 'Message', '__version__']
---
+++ py_grpcio/__meta__.py
@@ -0,0 +1,3 @@
+from typing import Final
+
+__version__: Final[str] = '1.4.4'
